48378e75c9315c558086f852751231ac22743090
deepesh_dhakal <deepesh@naphix.com.au>
2022-07-03 12:11:36 +0900

    refactored code for performance

--- edit.php
+++ edit.php
@@ -1,7 +1,7 @@
 <?php include('header.php'); ?>
 <div class="container">
 	<div id="alert_outer" class="alert alert-warning alert-dismissible fade show" role="alert">
-		<strong>Holy guacamole!</strong> You should check in on some of those fields below.
+		<span id="alert_inner"></span>
 		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 	</div>
 	<form>
@@ -16,7 +16,7 @@
 			</div>
 		</div>
 		<div class="form-row">
-			<textarea id="mytextarea">Hello, World!</textarea>
+			<textarea id="mytextarea" rows="10" cols="100"></textarea>
 		</div>
 		<div class="form-row">
 			<div class="form-group col-md-6">
@@ -33,50 +33,103 @@
 
 
 <script type="text/javascript">
+	const queryParams = new URLSearchParams(window.location.search);
+	const id = queryParams.get('id');
+
 	const inputTitle1 = document.getElementById('inputTitle1');
 	const inputSlug1 = document.getElementById('inputSlug1');
 	const mytextarea = document.getElementById('mytextarea');
 	const publishStatus = document.getElementById('publishStatus');
 	const postSubmitBtn = document.getElementById('postSubmitBtn');
-	const notification_box = document.getElementById('alert_outer');
-	notification_box.style.display = "none";
+	const alert_outer = document.getElementById('alert_outer');
+	const notification_message = document.getElementById('alert_inner');
+	alert_outer.style.display = "none";
 
+	if (id) {
+		// console.log(id);
+		//update an already existing post
+		let url = `https://wprestapi.test/wp-json/wp/v2/posts/${id}`;
+		let params = `?_fields=id,title,content,slug,status`;
+		// fetch('https://wprestapi.test/wp-json/wp/v2/posts/91?_fields=id,title,slug,status')
+		fetch(url + params)
+			.then(response => response.json())
+			.then(data => {
+				// console.log(data)
+				inputTitle1.value = data.title.rendered;
+				publishStatus.value = data.status;
+				mytextarea.value = data.content.rendered;
+				inputSlug1.value = data.slug;
+			})
 
-	postSubmitBtn.addEventListener('click', (e) => {
-		e.preventDefault();
-		if (inputTitle1.value && inputSlug1.value && mytextarea.value && publishStatus.value) {
-			fetch('https://wprestapi.test/wp-json/wp/v2/posts', {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-						'Authorization': 'Basic ' + btoa('deepyes02:oH1r sDnI xT7v ilc1 YBfh DUOw')
-					},
-					body: JSON.stringify({
-						"title" : inputTitle1.value,
-						"content" : mytextarea.value,
-						"slug"		: inputSlug1.value,
-						"status"	: publishStatus.value
-					}),
-				})
-				.then(response => {
-					if (response.ok == false) {
-						notification_box.style.display = "block";
-						notification_box.innerHTML = "<h2>Response invalid please try again</h2>";
-					}
-					console.log(response);
-					return response.json()
-				})
-				.then(data => {
-					notification_box.style.display = "block";
-					notification_box.innerHTML = "<h2>Success, data posted</h2>";
-					console.log(data);
-				})
-		} else {
-			notification_box.style.display = "block";
-			notification_box.innerHTML = "<h2>Please provide all input fields</h2>";
-		}
+		postSubmitBtn.addEventListener('click', (e) => {
+			e.preventDefault();
+			// console.log('clicked');
+			if (inputTitle1.value && inputSlug1.value && mytextarea.value && publishStatus.value) {
+				fetch(url + params, {
+						method: 'PUT',
+						headers: {
+							'Content-Type': 'application/json',
+							'Authorization': 'Basic ' + btoa('deepyes02:oH1r sDnI xT7v ilc1 YBfh DUOw')
+						},
+						body: JSON.stringify({
+							"title": inputTitle1.value,
+							"content": mytextarea.value,
+							"slug": inputSlug1.value,
+							"status": publishStatus.value
+						}),
+					})
+					.then(response => {
+						if (response.ok == false) {
+							alert_outer.style.display = "block";
+							notification_message.innerHTML = "<h2>Response invalid please try again</h2>";
+						}
+						console.log(response);
+						return response.json()
+					})
+					.then(data => {
+						alert_outer.style.display = "block";
+						notification_message.innerHTML = "<h2>Success, data uploaded</h2>";
+						console.log(data);
+					})
+			}
+		})
+	} else {
+		postSubmitBtn.addEventListener('click', (e) => {
+			e.preventDefault();
+			if (inputTitle1.value && inputSlug1.value && mytextarea.value && publishStatus.value) {
+				fetch('https://wprestapi.test/wp-json/wp/v2/posts', {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json',
+							'Authorization': 'Basic ' + btoa('deepyes02:oH1r sDnI xT7v ilc1 YBfh DUOw')
+						},
+						body: JSON.stringify({
+							"title": inputTitle1.value,
+							"content": mytextarea.value,
+							"slug": inputSlug1.value,
+							"status": publishStatus.value
+						}),
+					})
+					.then(response => {
+						if (response.ok == false) {
+							alert_outer.style.display = "block";
+							notification_message.innerHTML = "<h2>Response invalid please try again</h2>";
+						}
+						console.log(response);
+						return response.json()
+					})
+					.then(data => {
+						alert_outer.style.display = "block";
+						notification_message.innerHTML = "<h2>Success, data posted</h2>";
+						console.log(data);
+					})
+			} else {
+				alert_outer.style.display = "block";
+				notification_message.innerHTML = "<h2>Please provide all input fields</h2>";
+			}
 
-	})
+		})
+	}
 </script>
 
 
--- index.php
+++ index.php
@@ -1,4 +1,48 @@
 <?php include('header.php'); ?>
 <div id="root"></div>
-<script src="script.js" type="text/javascript"></script>
+<script type="text/javascript">
+	document.title = "REST API FRONTEND"
+	const root = document.querySelector('#root');
+	fetch('https://wprestapi.test/wp-json?_fields=name,description')
+		.then((data) => {
+			if (data.status == 200) {
+				return data.json()
+			} else console.log('Not success, please try again')
+		})
+		.then((data) => {
+			// console.log(data);
+			root.innerHTML = `<h1>${data.name}</h2>`;
+			root.innerHTML += `<h3>${data.description}</h3>`;
+		})
+		.catch(error => root.innerHTML = error)
+	// ?_fields=id,date,title,excerpt,link,featured_media,fimg_url
+	fetch('https://wprestapi.test/wp-json/wp/v2/posts?_fields=id,date,title,excerpt,link,featured_media,fimg_url')
+		.then((data) => {
+			if (data.status == 200) {
+				return data.json()
+			} else console.log('Not success, please try again')
+		})
+		.then((data) => {
+			// ._embedded['wp:featuredmedia']['0'].source_url
+			// console.log(data)
+			root.innerHTML += `<ul>`;
+			data.forEach(element => {
+				// console.log(element);
+				let postID = element.id;
+				let published = new Date(element.date);
+				root.innerHTML += `<div class="card mt-5" style="width: 18rem;">
+				<div class="card-body">
+				<img src="${element.fimg_url}" height="200" width="200">
+					<h5 class="card-title">${element.title.rendered}</h5>
+					<h6 class="card-subtitle mb-2 text-muted">${published.toLocaleDateString()} at ${published.toLocaleTimeString()}</h6>
+					${element.excerpt.rendered}
+					<a href="/single.php?id=${postID}" class="card-link">View Page</a>
+					<a href="/edit.php?id=${postID}" class="card-link">Edit Page</a>
+				</div>
+			</div>
+			`;
+			});
+			root.innerHTML += `</ul>`;
+		});
+</script>
 <?php include('footer.php'); ?>
